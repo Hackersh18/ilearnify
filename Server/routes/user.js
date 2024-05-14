dad51c6a047cc6ee195170f13c9db001423d4e59
Nitish Chauhan <nitishchauhan9090@gmail.com>
2024-05-14 22:24:42 +0530

    signup

---
+++ Server/routes/user.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const router = express.Router()
+const {
+    // login,
+    signup,
+    // sendotp,
+    // changePassword,
+} = require("../controllers/Auth")
+
+
+const { auth } = require("../middlewares/auth")
+
+
+
+// Route for user signup
+router.post("/signup", signup)
+
+
+
+
+
+
+// Export the router for use in the main application
+module.exports = router
